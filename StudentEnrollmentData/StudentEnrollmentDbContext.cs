9e2e6c2c27f2c36b39942cc814ff98c63f1e0dcb
Felipe Morales <felipe.moralescandia@gmail.com>
2023-08-08 12:16:00 -0400

    Agregado crud course
    Signed-off-by: Felipe Morales <felipe.moralescandia@gmail.com>

--- StudentEnrollmentData/StudentEnrollmentDbContext.cs
+++ StudentEnrollmentData/StudentEnrollmentDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using StudentEnrollmentData.Configuration;
 using System;
 using System.Collections.Generic;
@@ -26,6 +28,28 @@ namespace StudentEnrollmentData
         public DbSet<Course> Courses { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
+
+        public class StudentEnrollmentDbContextFactory : IDesignTimeDbContextFactory<StudentEnrollmentDbContext>
+        {
+            public StudentEnrollmentDbContext CreateDbContext(string[] args)
+            {
+                // Get environment
+                //string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                // Build config
+                IConfiguration config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                // Get connection string
+                var optionsBuilder = new DbContextOptionsBuilder<StudentEnrollmentDbContext>();
+                var connectionString = config.GetConnectionString("SchoolDbConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+                return new StudentEnrollmentDbContext(optionsBuilder.Options);
+            }
+        }
+
 
     }
 }
